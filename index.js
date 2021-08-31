69814b2fa071c9be37c79291f5a08e80841a72cc
Yoshimuro <72494754+Yoshimuro@users.noreply.github.com>
2021-08-31 12:27:49 +0800

    try to fix styles

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express")
+const ejs = require("ejs")
+const path = require('path')
+const sqlite3 = require("sqlite3").verbose();
+
+const app = express();
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join((__dirname, "public"))))
+app.set("views",path.join(__dirname,"views"))
+
+const db_name = path.join(__dirname,"data","apptest.db")
+const db = new sqlite3.Database(db_name, err => {
+    if(err){
+        return console.error(err.message)
+    }
+    console.log("Successful connection to the database 'apptest.db'");
+});
+
+/*const sql_create = `CREATE TABLE IF NOT EXISTS Books(
+    Book_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Title VARCHAR(100) NOT NULL,
+    Author VARCHAR(100) NOT NULL,
+    Comments TEXT
+);`
+db.run(sql_create,err => {
+    if(err){
+        return console.log(err.message)
+    }
+    console.log("Successful creation of the 'Books' table")
+})
+
+const sql_insert = `INSERT INTO Books (Book_ID, Title, Author, Comments) VALUES
+(1, 'Mrs. Bridge', 'Evan S. Connell', 'First in the serie'),
+(2, 'Mr. Bridge', 'Evan S. Connell', 'Second in the serie'),
+(3, 'L''ingénue libertine', 'Colette', 'Minne + Les égarements de Minne');`
+
+db.run(sql_insert, err => {
+    if(err){
+        return console.log(err.message)
+    }
+    console.log("Successful creation of 3 books")
+})*/
+
+
+
+app.listen(3000, () => { {
+    console.log("Server has started (http://localhost:3000/)!");
+}});
+
+app.get("/",(req,res) => { {
+   // res.send("Hello world...")
+    res.render("index")
+}})
+app.get("/about",(req,res) =>{ {
+    res.render("about")
+}} )
+
+app.get("/data",(req,res) => {
+    const test = {
+        title: "test",
+        items:["one","two","three"]
+    };
+    res.render("data",{model:test})
+})
+
+app.get("/books",(req,res) => {
+    const sql = "SELECT * FROM Books ORDER BY Title"
+    db.all(sql,[],(err,rows)=>{
+        if(err){
+            return console.error(err.message)
+        }
+        res.render("books",{model:rows})
+    })
+})
+
+app.get("/edit/:id",(req,res) =>{
+    const id = req.params.id
+    const sql = "SELECT * FROM Books WHERE Book_ID = ?"
+    db.get(sql, id, (err,row)=> {
+        if (err){
+            return console.log(err.message)
+        }
+        res.render("edit",{model:row})
+    })
+})
+
